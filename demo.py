6c02c9e287499eaee583643cefd9b0a48a981bde
Aditya kasaudhan <aditya259@amityonline.com>
2025-05-19 12:28:00 +0530

    new change

--- demo.py
+++ demo.py
@@ -1,6 +1,19 @@
 age =25
 print(age)
 
+a = int(input("enter num :"))
+b = int(input("enter num :"))
+print(a*b)
+
+age = int(input("enter age :"))
+
+if age!=18:
+    print("i am eligible")
+
+else:
+    print("i am not eligible")
+
+
 
 
     
